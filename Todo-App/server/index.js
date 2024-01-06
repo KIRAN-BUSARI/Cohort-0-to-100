bfeab0ab8282febc57c3ced10b60b36035b84f46
BusariKiranKumar <busarikirankumar@KiranKumars-MacBook-Air.local>
2024-01-06 23:31:30 +0530

    Added the final backend commit

--- Todo-App/server/index.js
+++ Todo-App/server/index.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { createTodo, updateTodo } from "./types";
-import { todo } from "./db";
-PORT = 8000;
+import { createTodo, updateTodo } from "./types.js";
+import { todo } from "./db.js";
+import { config } from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+config();
+
+const PORT = process.env.PORT || 8001;
 
 const app = express();
-
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.post("/todo", function (req, res) {
+app.post("/todo", async function (req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.success) {
@@ -17,24 +23,44 @@ app.post("/todo", function (req, res) {
         return;
     }
 
-    todo
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.status(200).json({
+        message: "Todo created successfully"
+    })
 })
 
-app.get("/todos",function (req, res) {
-    
+app.get("/todos", async function (req, res) {
+    const todos = await todo.findOne({})
+    // console.log(todos);
+    res.status(200).json(todos)
 })
 
-app.put("/completed",function (req, res) {
+app.put("/completed", async function (req, res) {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if (!parsedPayload.success) {
         res.status(411).json({
-            message: "You sent a wrong inputs"
+            message: "Invalid user inputs"
         })
         return;
     }
+
+    await todo.update({
+        _id: req.body._id
+    }, {
+        completed: true
+    })
+
+    res.status(200).json({
+        message: "Todo marked as completed"
+    })
 })
 
-app.listen(PORT, (req, res) => {
+app.listen(8000, (req, res) => {
     console.log(`Server is running on port ${PORT}`)
 });
--- Todo-App/server/types.js
+++ Todo-App/server/types.js
@@ -1,15 +1,15 @@
 import z from "zod";
 
-const createTodo = {
+const createTodo = z.object({
     title: z.string(),
     description: z.string(),
-}
+})
 
 const updateTodo = {
     id: z.string(),
 }
 
-module.exports = {
-    createTodo: createTodo,
-    updateTodo: updateTodo
+export {
+    createTodo,
+    updateTodo
 }
