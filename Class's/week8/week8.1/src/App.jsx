578c9587ff07163e5f10542cc640dbac980c7ec1
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-03-11 14:01:17 +0530

    Started zerodha landing page

---
+++ Class's/week8/week8.1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { RevenueCard } from './components/RevenueCard'
+function App() {
+
+  return (
+    <div className='flex'>
+      <RevenueCard />
+      <RevenueCard />
+      <RevenueCard />
+    </div>
+  )
+}
+
+export default App
