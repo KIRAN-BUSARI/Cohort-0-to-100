16a6a5512130f21884bffb7203f718bb7a92107f
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-03-12 22:03:14 +0530

    Add the postgres class

--- Class's/week10/week10.1/src/index.ts
+++ Class's/week10/week10.1/src/index.ts
@@ -2,6 +2,11 @@ import { Client } from "pg";
 
 const client = new Client({
     connectionString: "postgresql://kiranbusari2208:kjxt6nCfpXQ7@ep-super-firefly-82552384.us-east-2.aws.neon.tech/Test?sslmode=require"
+    // database: "test",
+    // user: "kiranbusari2208",
+    // password: "kjxt6nCfpXQ7",
+    // host: "ep-super-firefly-82552384.us-east-2.aws.neon.tech",
+    // port: 5432
 });
 
 
@@ -14,14 +19,15 @@ async function createUserTable() {
     //     password VARCHAR(255) NOT NULL,
     //     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     // );
+    // INSERT INTO users (
+    //     username, email, password
+    // ) VALUES (
+    //     'kiran1', 'kiran1@k.com', 'kiran123'
+    // );
     const result = await client.query(`
-    INSERT INTO users (
-        username, email, password
-    ) VALUES (
-        'kiran1', 'kiran1@k.com', 'kiran123'
-    );
+    SELECT * FROM users;
     `)
-    console.log(result);
+    console.log(result.rows);
 }
 
 createUserTable();
